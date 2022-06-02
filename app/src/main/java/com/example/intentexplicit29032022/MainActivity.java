81cd54a1355eff6cd6ecbdcdb1ddd457f8bb3fda
JoseNguyen <abc@example.com>
2022-06-02 19:00:08 +0700

    send data Array

--- app/src/main/java/com/example/intentexplicit29032022/MainActivity.java
+++ app/src/main/java/com/example/intentexplicit29032022/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.intentexplicit29032022;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 import java.util.Random;
@@ -13,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     String[] arrNameImages;
     ImageView imgRandom,imgPick;
-    int  valueImagRandom;
+    int valueImgRandom;
     Random random;
 
     @Override
@@ -25,8 +28,39 @@ public class MainActivity extends AppCompatActivity {
         imgRandom = findViewById(R.id.imgRandom);
         random = new Random();
         arrNameImages = getResources().getStringArray(R.array.arr_images);
-       int indexRandom= random.nextInt(arrNameImages.length);
-       valueImagRandom = getResources().getIdentifier(arrNameImages[indexRandom],"drawable",getPackageName());
-       imgRandom.setImageResource(valueImagRandom);
+        RandomImage(imgRandom);
+        imgPick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,GalleryImageActivity.class);
+                intent.putExtra("array",arrNameImages);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case    R.id.item_random:
+                RandomImage(imgRandom);
+                break;
+        }
+        return true;
+    }
+    private void RandomImage(ImageView imageView){
+        int indexRandom= random.nextInt(arrNameImages.length);
+        valueImgRandom = getResources().getIdentifier(
+                arrNameImages[indexRandom],
+                "drawable",
+                getPackageName()
+        );
+        imageView.setImageResource(valueImgRandom);
     }
 }
